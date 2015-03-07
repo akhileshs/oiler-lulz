fd3f72695bd05850186bcd5c644fce76261f8e35
akhileshsrikanth <akhilesh.srikanth@gmail.com>
2015-03-07 17:09:51 +0530

    even fibo sum

---
+++ src/oiler_lulz/problem2.clj
@@ -0,0 +1,16 @@
+(ns oiler.problem2
+  (:use oiler.templateFns))
+
+(defn fib-range [max]
+  (cond
+    (= max 0) (quote (0))
+    (= max 1) (quote (0 1))
+    :else
+      (loop [r '(0 1), n 1, p-2 0, p-1 1]
+        (if (< n max)
+          (let [f (+ p-2 p-1)]
+            (recur (concat r (list f)) (+ n 1) p-1 f))
+          r))))
+
+(defn oiler-problem2 []
+  (reduce + (for [f (fib-range 50) :while (< f 4000000) :when (even? f)] f)))
