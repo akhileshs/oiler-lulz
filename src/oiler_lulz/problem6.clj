8da629a45b9677998c240c1e0b2e00518c1c81f7
akhileshsrikanth <akhilesh.srikanth@gmail.com>
2015-03-11 23:58:53 +0530

    simple clj practice

---
+++ src/oiler_lulz/problem6.clj
@@ -0,0 +1,12 @@
+(ns oiler.problem6
+  (:use oiler.templateFns))
+
+(defn sum-of-squares [ns]         ;; return sum of squares of nos in collection
+  (reduce + (map sqr (ns))))
+
+(defn square-of-sums [ns]         ;; return square of sum of nos in collection
+  (sqr (reduce + ns)))
+
+(defn oiler-problem6 []
+  (- (square-of-sums (range 101)) (sum-of-squares (range 101))))
+
