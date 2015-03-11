9419779d8e945e6fba1fc501d673dfc4cb13f85f
akhileshsrikanth <akhilesh.srikanth@gmail.com>
2015-03-11 23:59:43 +0530

    prime jazz

---
+++ src/oiler_lulz/problem7.clj
@@ -0,0 +1,18 @@
+(ns oiler.problem7
+  (:use oiler.templateFns))
+
+(defn next-prime [n]
+  "returns next prime after n"
+  (loop [n (inc n)]
+    (if (prime? n) n
+      (recur (inc n)))))
+
+(defn nth-prime [n]
+  "returns nth prime"
+  (loop [i n, prime 1]
+    (if (zero? i) prime
+      (recur (dec i) (next-prime prime)))))
+
+(defn oiler-problem7 []
+  (nth-prime 10001))
+
